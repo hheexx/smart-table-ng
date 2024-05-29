42b96a350035fb79949fb6f85df4086d7d9ebaf0
Boris Prpic <hexboris@gmail.com>
2024-05-29 23:14:27 +0200

    remove dist

--- dist/src/factories.d.ts
+++
@@ -1,6 +0,0 @@
-import { TableState } from './table-state';
-import { SmartTable } from './smart-table.service';
-import { SmartTableExtension } from 'smart-table-core';
-import { ObservableInput } from 'rxjs/index';
-export declare const from: <T>(data: ObservableInput<T[]>, tableState?: TableState, ...extensions: SmartTableExtension<T>[]) => SmartTable<T>;
-export declare const of: <T>(data: T[], tableState?: TableState, ...extensions: SmartTableExtension<T>[]) => SmartTable<T>;
--- dist/src/table-state.d.ts
+++
@@ -1,7 +0,0 @@
-import { TableState as ITableState } from 'smart-table-core';
-export declare class TableState implements ITableState {
-    filter: {};
-    search: {};
-    slice: {};
-    sort: {};
-}
